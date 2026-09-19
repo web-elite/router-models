51ad890a1158e2ef0d55eece1b10e1ac5661aaa8
Alireza <webelitee@gmail.com>
2026-09-19 14:50:51 +0330

    change detect free models emoji

--- src/sidebar.ts
+++ src/sidebar.ts
@@ -351,7 +351,7 @@ export class RouterSidebar
         <button id="btn-import" class="icon-btn" title="Import providers from JSON">&#10515;</button>
         <button id="btn-export" class="icon-btn" title="Export providers to JSON">&#10514;</button>
         <button id="btn-refresh" class="icon-btn" title="Refresh all providers">&#8635;</button>
-        <button id="btn-free" class="icon-btn" title="Detect free models">&#127873;</button>
+        <button id="btn-free" class="icon-btn" title="Detect free models">&#127379;</button>
         <button id="btn-settings" class="icon-btn" title="Include / exclude settings">&#9881;</button>
     </header>
 
